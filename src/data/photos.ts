fefbabddad744d00fd707cd7ae67dd7e318dba93
jkranz-rk <jakranz33+rk@gmail.com>
2026-05-29 14:47:31 -0600

    Adding some photos to assets and the gallery

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -5,8 +5,8 @@ export type Photo = {
 };
 
 export const photos: Photo[] = [
-  { src: '/images/placeholder.svg', alt: 'Photo 1' },
-  { src: '/images/placeholder.svg', alt: 'Photo 2' },
+  { src: '/images/photos/DSC_0095.JPG', alt: 'Image of a tree on Pennsylvania Mountain' },
+  { src: '/images/photos/DSC_0099.JPG', alt: 'Image of hiking trail at the base of Pennsylvania Mountain looking out at the mountain range on the horizon' },
   { src: '/images/placeholder.svg', alt: 'Photo 3' },
   { src: '/images/placeholder.svg', alt: 'Photo 4' },
   { src: '/images/placeholder.svg', alt: 'Photo 5' },
